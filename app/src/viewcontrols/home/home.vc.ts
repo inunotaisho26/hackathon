194e439d2ad66fca47e05e1e14c237ea8fc8ee2e
Matt Morgan <m@getplatypi.com>
2016-01-23 16:13:05 -0600

    updated home page

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -23,8 +23,9 @@ export default class HomeViewControl extends BaseViewControl {
                         lg: "http://mobileimages.lowes.com/product/converted/020536/020536017679lg.jpg",
                         xl: "http://mobileimages.lowes.com/product/converted/020536/020536017679.jpg"
                     },
-                    description: 'Camellia (Camellia japonica) is a great shrub for a shady area, thanks to the white variegation accenting dark-green foliage. Hardy in zones 8-10, camellia thrives in full to light shade with adequate moisture. Camellia grows at a moderate pace, and can reach 8 to 10 feet. Camellias should be planted in the shade in organic, somewhat acidic, semi-moist but well drained soil. If the soil is not well drained, it can cause the roots to rot'
-                }]
+                    description: 'Camellia (Camellia japonica) is a great shrub for a shady area, thanks to the white variegation accenting dark-green foliage. Hardy in zones 8-10, camellia thrives in full to light shade with adequate moisture. Camellia grows at a moderate pace, and can reach 8 to 10 feet. Camellias should be planted in the shade in organic, somewhat acidic, semi-moist but well drained soil. If the soil is not well drained, it can cause the roots to rot.'
+                }],
+                related: 'Japanese maple, flowering dogwood, mahonia, Aucuba, Japanese yew, plum yew, Japanese aralia, hydrangea, azalea'
             }, {
                 title: 'Perennials',
                 description: '',
@@ -45,8 +46,10 @@ export default class HomeViewControl extends BaseViewControl {
                         lg: "http://mobileimages.lowes.com/product/converted/020536/020536327020lg.jpg",
                         xl: "http://mobileimages.lowes.com/product/converted/020536/020536327020.jpg"
                     },
-                    description: 'Asparagus Fern (Asparagus densiflorus) is an evergreen perennial plant, closely related to the vegetable asparagus. Asparagus fern cannot tolerate frost, which makes it a superb choice for gardening in the south central region. It does not require much water, making it a good plant to grow under glass. It is fast growing, reaching a maximum height of 24 inches.'
-                }]
+                    description: 'Asparagus Fern (Asparagus densiflorus) is an evergreen perennial plant, closely related to the vegetable asparagus. Asparagus fern cannot tolerate frost, which makes it a superb choice for gardening in the south central region. It does not require much water, making it a good plant to grow under glass. It is fast growing, reaching a maximum height of 24 inches.',
+                    related: ''
+                }],
+                related: 'bleeding heart, yellow corydalis, ferns, hosta, coralbells, Lenten rose, pansy, oxalis, columbine, gardenia, woodland phlox, plumbago'
             }, {
                 title: 'Annuals',
                 description: '',
@@ -59,7 +62,8 @@ export default class HomeViewControl extends BaseViewControl {
                         xl: "http://mobileimages.lowes.com/product/converted/014934/014934586097.jpg"
                     },
                     description: 'Caladiums (Caladium x hortulanum) grow from tubers and greatly complement other shade-loving plants such as ferns and impatiens. Mix several caladium varieties in a container for a colorful display of pink, red, rose, chartreuse, green, and white. Hardy in Zones 8–10, the tubers can be lifted and stored over winter in colder climates.'
-                }]
+                }],
+                related: 'coleus, impatiens, wax begonia, wishbone flower, elephant’s ears, ginger'
             }]
         }
     };
