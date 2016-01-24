ba2bd395565029ad5fd9466e48cec564e935c39c
Will Johnston <will@getplatypi.com>
2016-01-23 21:24:11 -0600

    ability to get full product

--- app/src/services/product/product.svc.ts
+++ app/src/services/product/product.svc.ts
@@ -1,8 +1,9 @@
 import {async, register} from 'platypus';
 import BaseService from '../base/base.svc';
+import Link from '../link/link.svc';
 
 export default class ProductService extends BaseService {
-    constructor() {
+    constructor(private links: Link) {
         super('product');
     }
 
@@ -10,12 +11,25 @@ export default class ProductService extends BaseService {
         return this.get('keyword' + this.toQuery(data));
     }
 
-    byId(data: IProductById): async.IThenable<models.IProductList> {
-        return this.get('productId' + this.toQuery(data));
+    byId(data: IProductById): async.IThenable<Array<models.IProduct>> {
+        return this.get('productId' + this.toQuery(data)).then((list: models.IProductList) => {
+            return this.utils.mapAsync((product) => {
+                return this.Promise.all([
+                    this.links.specification(product.links),
+                    this.links.requiredItems(product.links)
+                ]).then((links: Array<any>) => {
+                    product.specification = links[0];
+                    product.requiredItems = links[1];
+                    return product;
+                });
+            }, list.productList);
+        });
     }
 }
 
-register.injectable('product-svc', ProductService);
+register.injectable('product-svc', ProductService, [
+   Link
+]);
 
 interface IProductBy {
     storeNumber?: number;
