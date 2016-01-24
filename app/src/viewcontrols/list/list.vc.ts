c220cb0d225f16b00ddf481da52b967fcba8ec03
Will Johnston <will@getplatypi.com>
2016-01-23 22:06:52 -0600

    context for lists page

--- app/src/viewcontrols/list/list.vc.ts
+++ app/src/viewcontrols/list/list.vc.ts
@@ -1,10 +1,29 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import List from '../../services/list/list.svc';
+import {LIST_NAME} from '../../references/references';
 
 export default class ListViewControl extends BaseViewControl {
     templateString: string = require('./list.vc.html');
 
-    context: any = {};
+    context = {
+        products: <Array<models.IListItem>>null
+    };
+
+    constructor(private lists: List) {
+        super();
+    }
+
+    initialize(): void {
+        this.lists.list(LIST_NAME).then((list) => {
+            return this.lists.items(list.id);
+        }).then((items) => {
+            this.context.products = items.list;
+            console.log(items.list);
+        });
+    }
 }
 
-register.viewControl('list-vc', ListViewControl);
+register.viewControl('list-vc', ListViewControl, [
+    List
+]);
